8b4f40a231bfba1de04f872335815d7667e749fe
Drabinko Artur <artur.drabinko@gmail.com>
2018-02-25 15:32:09 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  function getAllPrimesWithSum(b){
+
+    PrimesObject = {};
+    for (let i = 2; i <= b; i++) {
+      
+      if(b % i == 0){
+        if(PrimesObject[i]){
+          PrimesObject[i]++;
+        }else{
+          PrimesObject[i] = 1;
+        }
+        b /= i;
+        i = 1;
+      }
+
+    }
+    return PrimesObject;
+  }
+
+  primes = getAllPrimesWithSum(base);
+  arrMaxSumNum = [];
+ 
+  for (const value in primes) {
+  
+    let num = number;
+    let rezult = 0;
+    while(num >= value){
+      num = Math.floor( num / value );
+      rezult += num;
+    }
+ 
+    arrMaxSumNum.push( Math.floor( rezult / primes[value]));
+
+  }
+
+  return Math.min.apply(null, arrMaxSumNum);
 }
